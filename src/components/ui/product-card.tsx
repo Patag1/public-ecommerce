51a069dc1178687ea64eca0c1b77573fd958f4f7
Augusto <augustosasso97@gmail.com>
2023-08-29 01:27:05 -0300

    image fix

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -44,8 +44,7 @@ const ProductCard: FC<ProductCardProps> = ({ data }) => {
         <Image
           src={data?.images?.[0]?.url}
           alt={data?.name}
-          width={1}
-          height={1}
+          fill
           className="w-full aspect-square object-cover rounded-md"
         />
         <div className="absolute bottom-5 w-full px-6 opacity-0 group-hover:opacity-100 transition-opacity ease-in-out">
