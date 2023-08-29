fd120ca4c0a5d03247d23855188fd52d6553443e
Augusto <augustosasso97@gmail.com>
2023-08-29 01:28:41 -0300

    grid fix

--- src/app/(routes)/category/[catId]/page.tsx
+++ src/app/(routes)/category/[catId]/page.tsx
@@ -43,7 +43,7 @@ const page: FC<pageProps> = async ({ params, searchParams }) => {
             </div>
             <div className="mt-6 lg:mt-0 lg:col-span-4">
               {products.length === 0 && <NoResults />}
-              <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-1 gap-4">
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {products.map((p, i) => (
                   <ProductCard key={i} data={p} />
                 ))}
